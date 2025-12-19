35537314bdc0255f153cf6a7f7b6e32fafa4cf08
Atharv Kadam <atharvvikaskadam3333@gmail.com>
2025-12-19 11:17:45 +0530

    Time: 33 ms (60.49%), Space: 59.1 MB (56.5%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,11 +2,14 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
 
         for (int i = 0; i < nums.length - 2; i++) {
 
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            // Skip duplicate i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
             int left = i + 1;
             int right = nums.length - 1;
@@ -15,24 +18,28 @@ class Solution {
                 int sum = nums[i] + nums[left] + nums[right];
 
                 if (sum == 0) {
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicate left
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+
+                    // Skip duplicate right
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
 
                     left++;
                     right--;
-
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
 
                 } else if (sum < 0) {
                     left++;
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-
                 } else {
                     right--;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
                 }
             }
         }
-        return res;
+        return result;
     }
 }
