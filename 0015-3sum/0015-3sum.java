b7468365059839d56a149d37ad317e6718de5dfd
Atharv Kadam <atharvvikaskadam3333@gmail.com>
2025-12-19 11:17:36 +0530

    Time: 43 ms (23.33%), Space: 59.2 MB (32.83%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,14 +2,11 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
 
         for (int i = 0; i < nums.length - 2; i++) {
 
-            // Skip duplicate i
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int left = i + 1;
             int right = nums.length - 1;
@@ -18,28 +15,24 @@ class Solution {
                 int sum = nums[i] + nums[left] + nums[right];
 
                 if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    // Skip duplicate left
-                    while (left < right && nums[left] == nums[left + 1]) {
-                        left++;
-                    }
-
-                    // Skip duplicate right
-                    while (left < right && nums[right] == nums[right - 1]) {
-                        right--;
-                    }
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
                     left++;
                     right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
 
                 } else if (sum < 0) {
                     left++;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+
                 } else {
                     right--;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
                 }
             }
         }
-        return result;
+        return res;
     }
 }
