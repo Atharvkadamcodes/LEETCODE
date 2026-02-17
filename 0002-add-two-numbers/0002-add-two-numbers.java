496273f71a0d578ad7363239b75b58b31b492687
Atharv Kadam <atharvvikaskadam3333@gmail.com>
2026-02-17 22:03:06 +0530

    Time: 1 ms (99.74%), Space: 46.3 MB (90.75%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -12,33 +12,22 @@ import java.math.BigInteger;
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        StringBuilder s1 = new StringBuilder();
-        StringBuilder s2 = new StringBuilder();
-
-        while (l1 != null) {
-            s1.append(l1.val);
-            l1 = l1.next;
-        }
-
-        while (l2 != null) {
-            s2.append(l2.val);
-            l2 = l2.next;
-        }
-
-        s1.reverse();
-        s2.reverse();
-
-        BigInteger n1 = new BigInteger(s1.toString());
-        BigInteger n2 = new BigInteger(s2.toString());
-
-        String result = n1.add(n2).toString();
-
         ListNode dummy = new ListNode(0);
         ListNode curr = dummy;
 
-        for (int i = result.length() - 1; i >= 0; i--) {
-            curr.next = new ListNode(result.charAt(i) - '0');
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
             curr = curr.next;
+
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
 
         return dummy.next;
