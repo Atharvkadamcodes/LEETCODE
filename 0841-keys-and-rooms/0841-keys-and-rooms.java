9bf9deb3baa2f63d938a384a71869be6729cf815
Atharv Kadam <atharvvikaskadam3333@gmail.com>
2026-07-30 22:09:20 +0530

    Time: 5 ms (4.63%), Space: 45.9 MB (97.66%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,50 @@
+class Solution {
+    class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public void dfs(ArrayList<Edge>[] graph, int curr, boolean vis[]) {
+        vis[curr] = true;
+
+        for(int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if(!vis[e.dest]) {
+                dfs(graph, e.dest, vis);
+            }
+        }
+    }
+
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        ArrayList<Edge>[] graph = new ArrayList[rooms.size()];
+
+        for (int i = 0; i < rooms.size(); i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < rooms.size(); i++) {
+            List<Integer> room = rooms.get(i);
+            for(int j = 0; j < room.size(); j++) {
+                Edge e = new Edge(0, room.get(j));
+                graph[i].add(e);
+            }
+        }
+
+        boolean vis[] = new boolean[rooms.size()];
+
+        dfs(graph, 0, vis);
+
+        for(int i = 0; i < vis.length; i++) {
+            if(!vis[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
