4c50ae81a7bbe509dd79fba7751cdc00b083042b
Atharv Kadam <atharvvikaskadam3333@gmail.com>
2026-08-03 16:37:15 +0530

    Time: 2 ms (7.26%), Space: 44.7 MB (52.43%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,29 +1,31 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int pivot = -1;
-
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                pivot = i;
+        int checkPoint = -1;
+        for(int i = nums.length - 1; i >= 1; i--) {
+            if(nums[i] > nums[i - 1]) {
+                checkPoint = i - 1;
                 break;
             }
         }
 
-        if(pivot == -1) {
+        if (checkPoint == -1) {
             Arrays.sort(nums);
             return;
         }
 
-        for (int i = n - 1; i > pivot; i--) {
-            if (nums[i] > nums[pivot]) {
-                int temp = nums[i];
-                nums[i] = nums[pivot];
-                nums[pivot] = temp;
-                break;
+        int smallestIdx = checkPoint + 1;
+        for(int i = checkPoint + 1; i < nums.length; i++) {
+            if (nums[i] > nums[checkPoint] &&
+                nums[i] < nums[smallestIdx]) {
+
+                smallestIdx = i;
             }
         }
 
-        Arrays.sort(nums, pivot + 1, n);
+        int temp = nums[checkPoint];
+        nums[checkPoint] = nums[smallestIdx];
+        nums[smallestIdx] = temp;
+
+        Arrays.sort(nums, checkPoint + 1, nums.length);
     }
 }
