f45f072c9be696bd3bf7bebfd16cb131d37e005c
Atharv Kadam <atharvvikaskadam3333@gmail.com>
2026-08-16 17:37:47 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.73%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -7,14 +7,13 @@ class Solution {
         while(j >= 0) {
             if(i >= 0 && nums1[i] > nums2[j]) {
                 nums1[k] = nums1[i];
-                k--;
                 i--;
+                k--;
             } else {
                 nums1[k] = nums2[j];
-                k--;
                 j--;
+                k--;
             }
         }
-
     }
 }
