48b41619a0c76213823712cdf38aa68d3568bcb9
Atharv Kadam <atharvvikaskadam3333@gmail.com>
2026-08-20 19:58:16 +0530

    Time: 2 ms (92%), Space: 48 MB (20.65%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,9 +1,8 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        
         int j = 0;
 
-        for (int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) {
             if(nums[i] != 0) {
                 nums[j] = nums[i];
                 j++;
@@ -11,7 +10,7 @@ class Solution {
         }
 
         for(int i = j; i < nums.length; i++) {
-            nums[i] =0;
+            nums[i] = 0;
         }
     }
 }
